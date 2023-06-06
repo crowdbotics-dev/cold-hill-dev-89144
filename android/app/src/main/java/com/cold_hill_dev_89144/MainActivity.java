fdb26d1f2b8a1d31964c380de7a7b1c8b67125cb
Crowdbotics <engineering@crowdbotics.com>
2023-06-06 12:50:53 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/cold_hill_dev_89144/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cold_hill_dev_89144;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "cold_hill_dev_89144";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cold-hill-dev-89144",
+  url: "https://cold-hill-dev-89144.botics.co",
+  api: "https://cold-hill-dev-89144.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
